4f54dc03fe82fa69b18d8e3fcc86d26138f9aeb0
Oczane <oczane.rivera@gmail.com>
2017-11-13 14:40:33 -0500

    commiting changes

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,13 +1,24 @@
 'use strict';
 var express = require('express');
 var router=express();
+var models= require('../models')
+
+var Page = models.Page;
 
 router.get('/',function(req,res){
   res.redirect('..')
 });
 
 router.post('/',function(req,res){
-  res.send('posttt')
+console.log(req.body);
+ var page = Page.build({
+    title: req.body.title,
+    content: req.body.pageContent
+  });
+page.save().then(function successHandler(){
+  res.redirect('/');
+});
+
 });
 
 router.get('/add',function(req,res){
